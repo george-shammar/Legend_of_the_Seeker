b3b88885105d8507e8b8d7c9668963e9149608c0
george-shammar <george.gbenle@outlook.com>
2021-06-13 15:06:58 +0000

    Delete unused codes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
-import 'phaser';
-
-import { SimpleScene } from './scenes/simple-scene';
-
-const gameConfig = {
-  width: 800,
-  height: 600,
-  scene: SimpleScene
-};
-
-new Phaser.Game(gameConfig);
 
