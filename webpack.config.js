98f1e798b367f8d8a8e4a69c83242aece1000279
george-shammar <george.gbenle@outlook.com>
2021-06-13 21:35:39 +0000

    Refactor configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,27 @@
-const path = require('path');
-const CopyPlugin = require("copy-webpack-plugin");
+'use strict';
+
 const webpack = require('webpack');
+const path = require('path');
 
 module.exports = {
-  entry: {
-    app: './src/index.js'
-  },
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        include: path.resolve(__dirname, 'src/'),
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-                ['@babel/preset-env', { targets: "defaults" }]
-              ]
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        publicPath: '/build/',
+        filename: 'project.bundle.js'
+    },
+    module: {
+        rules: [
+          {
+            test: [ /\.vert$/, /\.frag$/ ],
+            use: 'raw-loader'
           }
-        }
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      }
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'CANVAS_RENDERER': JSON.stringify(true),
+            'WEBGL_RENDERER': JSON.stringify(true)
+        })
     ]
-  },
-
-  plugins: [
-    new CopyPlugin({
-        patterns: [
-          { from: path.resolve(__dirname, 'assets', '**', '*'),
-          to: path.resolve(__dirname, 'dist') }
-        ],
-      }),
-    new webpack.DefinePlugin({
-      'typeof CANVAS_RENDERER': JSON.stringify(true),
-      'typeof WEBGL_RENDERER': JSON.stringify(true)
-    }),
-  ],
-}
+};
