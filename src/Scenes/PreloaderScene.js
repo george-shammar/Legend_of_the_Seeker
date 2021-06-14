5487c3b07e582c4199d6a95768bb524529310fc3
george-shammar <george.gbenle@outlook.com>
2021-06-14 18:18:13 +0000

    Adopt ES6 convention

--- src/Scenes/PreloaderScene.js
+++ src/Scenes/PreloaderScene.js
@@ -11,17 +11,17 @@ export default class PreloaderScene extends Phaser.Scene {
 
   preload () {
     // add logo image
-    this.add.image(400, 200, 'logo');
+    this.add.image(650, 200, 'logo');
 
     // display progress bar
-    var progressBar = this.add.graphics();
-    var progressBox = this.add.graphics();
+    const progressBar = this.add.graphics();
+    const progressBox = this.add.graphics();
     progressBox.fillStyle(0x222222, 0.8);
     progressBox.fillRect(240, 270, 320, 50);
 
-    var width = this.cameras.main.width;
-    var height = this.cameras.main.height;
-    var loadingText = this.make.text({
+    const width = this.cameras.main.width;
+    const height = this.cameras.main.height;
+    const loadingText = this.make.text({
       x: width / 2,
       y: height / 2 - 50,
       text: 'Loading...',
