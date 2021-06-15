e6d20c5b9d649c9ad74e2b51b82e50c8c4b7ec4e
george-shammar <george.gbenle@outlook.com>
2021-06-15 10:35:19 +0000

    Add tomatoes

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -9,11 +9,11 @@ export default class GameScene extends Phaser.Scene {
     super('Game');
   }
 
-  create () {
+  create() {
     let player;
     let mud;
     let groundmiddle;
- 
+    let tomato;
     
 
     //Add background world.
@@ -21,12 +21,12 @@ export default class GameScene extends Phaser.Scene {
  
     //Add platform
     groundmiddle = this.physics.add.staticGroup();
-    groundmiddle.create(310, 188, 'groundmiddle').setScale(1).refreshBody();
-    groundmiddle.create(450, 188, 'groundmiddle').setScale(1).refreshBody();
-    groundmiddle.create(580, 188, 'groundmiddle').setScale(1).refreshBody();
-    groundmiddle.create(855, 188, 'groundmiddle').setScale(1).refreshBody();
-    groundmiddle.create(1200, 188, 'groundmiddle').setScale(1).refreshBody();
-    groundmiddle.create(1122, 187, 'groundmiddle').setScale(1).refreshBody();
+    groundmiddle.create(310, 188, 'groundmiddle');
+    groundmiddle.create(450, 188, 'groundmiddle');
+    groundmiddle.create(580, 188, 'groundmiddle');
+    groundmiddle.create(855, 188, 'groundmiddle');
+    groundmiddle.create(1200, 188, 'groundmiddle');
+    groundmiddle.create(1122, 187, 'groundmiddle');
 
 
     mud = this.physics.add.staticGroup();
@@ -34,64 +34,53 @@ export default class GameScene extends Phaser.Scene {
 
    
 
-
-    
-
     //Add player sprite.
-    player = this.physics.add.sprite(100, 450, 'girl');
+    player = this.physics.add.sprite(100, 450, 'girl').setScale(2);
     
     player.setBounce(0.2);
     player.setCollideWorldBounds(true);
+    player.body.setGravityY(300);
 
     //Add animation for player sprite.
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('girl', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
 
-    // this.anims.create({
-    //     key: 'left',
-    //     frames: this.anims.generateFrameNumbers('girl', { start: 0, end: 3 }),
-    //     frameRate: 10,
-    //     repeat: -1
-    // });
+    this.anims.create({
+        key: 'turn',
+        frames: [ { key: 'girl', frame: 4 } ],
+        frameRate: 20
+    });
 
-    // this.anims.create({
-    //     key: 'turn',
-    //     frames: [ { key: 'girl', frame: 4 } ],
-    //     frameRate: 20
-    // });
+    this.anims.create({
+      key: 'right',
+      frames: this.anims.generateFrameNumbers('girl', { start: 5, end: 8 }),
+      frameRate: 10,
+      repeat: -1
+    });
 
+    //Add fruits
+    tomato = this.physics.add.group({
+      key: 'tomato',
+      repeat: 11,
+      setXY: { x: 12, y: 0, stepX: 70 }
+    });
 
-    // this.anims.create({
-    //   key: 'right',
-    //   frames: this.anims.generateFrameNumbers('girl', { start: 5, end: 8 }),
-    //   frameRate: 10,
-    //   repeat: -1
-    // });
+    tomato.children.iterate(function (child) {
+      child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+    });
 
-    this.physics.add.collider(player, mud, groundmiddle);
-    // cursors = this.input.keyboard.createCursorKeys();
-    
+    //Set colliders
+    this.physics.add.collider(player, mud);
+    this.physics.add.collider(player, groundmiddle);
+    this.physics.add.collider(tomato, mud);
+    this.physics.add.collider(tomato, groundmiddle);
 
-    // if (cursors.left.isDown)
-    //     {
-    //         player.setVelocityX(-160);
+    cursors = this.input.keyboard.createCursorKeys();
 
-    //         player.anims.play('left', true);
-    //     }
-    //     else if (cursors.right.isDown)
-    //     {
-    //         player.setVelocityX(160);
+  }
 
-    //         player.anims.play('right', true);
-    //     }
-    //     else
-    //     {
-    //         player.setVelocityX(0);
-
-    //         player.anims.play('turn');
-    //     }
-
-    //     if (cursors.up.isDown && player.body.touching.down)
-    //     {
-    //         player.setVelocityY(-330);
-    //     }
-    }
 };
