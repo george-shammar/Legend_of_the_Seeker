7c1eaa14c5cf309720e3d30cb345fc2ac66269f0
george-shammar <george.gbenle@outlook.com>
2021-06-16 15:57:58 +0000

    Create menu class

--- src/Scenes/BattleScene.js
+++ src/Scenes/BattleScene.js
@@ -98,3 +98,53 @@ var MenuItem = new Phaser.Class({
     }
     
 });
+
+var Menu = new Phaser.Class({
+    Extends: Phaser.GameObjects.Container,
+    
+    initialize:
+            
+    function Menu(x, y, scene, heroes) {
+        Phaser.GameObjects.Container.call(this, scene, x, y);
+        this.menuItems = [];
+        this.menuItemIndex = 0;
+        this.heroes = heroes;
+        this.x = x;
+        this.y = y;
+    },     
+    addMenuItem: function(unit) {
+        var menuItem = new MenuItem(0, this.menuItems.length * 20, unit, this.scene);
+        this.menuItems.push(menuItem);
+        this.add(menuItem);        
+    },            
+    moveSelectionUp: function() {
+        this.menuItems[this.menuItemIndex].deselect();
+        this.menuItemIndex--;
+        if(this.menuItemIndex < 0)
+            this.menuItemIndex = this.menuItems.length - 1;
+        this.menuItems[this.menuItemIndex].select();
+    },
+    moveSelectionDown: function() {
+        this.menuItems[this.menuItemIndex].deselect();
+        this.menuItemIndex++;
+        if(this.menuItemIndex >= this.menuItems.length)
+            this.menuItemIndex = 0;
+        this.menuItems[this.menuItemIndex].select();
+    },
+    // select the menu as a whole and an element with index from it
+    select: function(index) {
+        if(!index)
+            index = 0;
+        this.menuItems[this.menuItemIndex].deselect();
+        this.menuItemIndex = index;
+        this.menuItems[this.menuItemIndex].select();
+    },
+    // deselect this menu
+    deselect: function() {        
+        this.menuItems[this.menuItemIndex].deselect();
+        this.menuItemIndex = 0;
+    },
+    confirm: function() {
+        // wen the player confirms his slection, do the action
+    }   
+});
