f75a678bcc61d71ec94950cd3c50709c0dc03682
george-shammar <george.gbenle@outlook.com>
2021-06-16 15:27:03 +0000

    Change box size for UI box

--- src/Scenes/UIScene.js
+++ src/Scenes/UIScene.js
@@ -12,14 +12,14 @@ export default class UIScene extends Phaser.Scene {
         this.graphics.lineStyle(1, 0xffffff);
         this.graphics.fillStyle(0x031f4c, 1); 
 
-        this.graphics.strokeRect(457, 500, 130, 100);
-        this.graphics.fillRect(457, 500, 130, 100);
+        this.graphics.strokeRect(457, 500, 100, 100);
+        this.graphics.fillRect(457, 500, 100, 100);
 
-        this.graphics.strokeRect(557, 500, 130, 100);
-        this.graphics.fillRect(557, 500, 130, 100);
+        this.graphics.strokeRect(557, 500, 100, 100);
+        this.graphics.fillRect(557, 500, 100, 100);
 
-        this.graphics.strokeRect(650, 500, 130, 100);
-        this.graphics.fillRect(650, 500, 130, 100);
+        this.graphics.strokeRect(650, 500, 100, 100);
+        this.graphics.fillRect(650, 500, 100, 100);
     }
 
 }  
