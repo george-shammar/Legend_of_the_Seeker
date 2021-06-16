3d16db0acedd6d8d74438a61e8e94ee035bcd030
george-shammar <george.gbenle@outlook.com>
2021-06-16 15:38:07 +0000

    Add Unit game object

--- src/Scenes/BattleScene.js
+++ src/Scenes/BattleScene.js
@@ -14,4 +14,32 @@ export default class BattleScene extends Phaser.Scene {
         this.scene.launch('UIScene');
     }
 
-}  
+}
+
+const Unit = new Phaser.Class({
+    Extends: Phaser.GameObjects.Sprite,
+ 
+    initialize:
+ 
+    function Unit(scene, x, y, texture, frame, type, hp, damage) {
+        Phaser.GameObjects.Sprite.call(this, scene, x, y, texture, frame)
+        this.type = type;
+        this.maxHp = this.hp = hp;
+        this.damage = damage; // default damage                
+    },
+    attack: function(target) {
+        target.takeDamage(this.damage);      
+    },
+    takeDamage: function(damage) {
+        this.hp -= damage;        
+    }
+});
+
+const Enemy = new Phaser.Class({
+    Extends: Unit,
+ 
+    initialize:
+    function Enemy(scene, x, y, texture, frame, type, hp, damage) {
+        Unit.call(this, scene, x, y, texture, frame, type, hp, damage);
+    }
+});
