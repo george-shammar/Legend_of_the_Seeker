d64c7e43066eb331509ebea2fcfb962f8d1c2934
george-shammar <george.gbenle@outlook.com>
2021-06-16 15:20:33 +0000

    Change x and y axis of UI box

--- src/Scenes/UIScene.js
+++ src/Scenes/UIScene.js
@@ -10,13 +10,16 @@ export default class UIScene extends Phaser.Scene {
     {
         this.graphics = this.add.graphics();
         this.graphics.lineStyle(1, 0xffffff);
-        this.graphics.fillStyle(0x031f4c, 1);        
+        this.graphics.fillStyle(0x031f4c, 1); 
+
         this.graphics.strokeRect(2, 150, 90, 100);
         this.graphics.fillRect(2, 150, 90, 100);
+
         this.graphics.strokeRect(95, 150, 90, 100);
         this.graphics.fillRect(95, 150, 90, 100);
-        this.graphics.strokeRect(188, 150, 130, 100);
-        this.graphics.fillRect(188, 150, 130, 100);
+
+        this.graphics.strokeRect(650, 500, 130, 100);
+        this.graphics.fillRect(650, 500, 130, 100);
     }
 
 }  
