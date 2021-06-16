e810153ad82e0dec251a15ad3f8a6169521bedf8
george-shammar <george.gbenle@outlook.com>
2021-06-16 15:24:46 +0000

    Change box size for UI box

--- src/Scenes/UIScene.js
+++ src/Scenes/UIScene.js
@@ -12,8 +12,8 @@ export default class UIScene extends Phaser.Scene {
         this.graphics.lineStyle(1, 0xffffff);
         this.graphics.fillStyle(0x031f4c, 1); 
 
-        this.graphics.strokeRect(457, 150, 90, 100);
-        this.graphics.fillRect(2, 150, 90, 100);
+        this.graphics.strokeRect(457, 500, 130, 100);
+        this.graphics.fillRect(457, 500, 130, 100);
 
         this.graphics.strokeRect(557, 500, 90, 100);
         this.graphics.fillRect(557, 500, 90, 100);
