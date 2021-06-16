c1bd7c40b9c7b84fd8d9c38f2dc74c8ea5bda8ed
george-shammar <george.gbenle@outlook.com>
2021-06-16 16:00:18 +0000

    Create menus

--- src/Scenes/BattleScene.js
+++ src/Scenes/BattleScene.js
@@ -145,6 +145,44 @@ var Menu = new Phaser.Class({
         this.menuItemIndex = 0;
     },
     confirm: function() {
-        // wen the player confirms his slection, do the action
+        // when the player confirms his slection, do the action
     }   
+});
+
+var HeroesMenu = new Phaser.Class({
+    Extends: Menu,
+    
+    initialize:
+            
+    function HeroesMenu(x, y, scene) {
+        Menu.call(this, x, y, scene);                    
+    }
+});
+ 
+var ActionsMenu = new Phaser.Class({
+    Extends: Menu,
+    
+    initialize:
+            
+    function ActionsMenu(x, y, scene) {
+        Menu.call(this, x, y, scene);   
+        this.addMenuItem('Attack');
+    },
+    confirm: function() {
+        // do something when the player selects an action
+    }
+    
+});
+ 
+var EnemiesMenu = new Phaser.Class({
+    Extends: Menu,
+    
+    initialize:
+            
+    function EnemiesMenu(x, y, scene) {
+        Menu.call(this, x, y, scene);        
+    },       
+    confirm: function() {        
+        // do something when the player selects an enemy
+    }
 });
