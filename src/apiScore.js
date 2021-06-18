81a8057c2699fdff9e93302504bf423e9f3354f0
george-shammar <george.gbenle@outlook.com>
2021-06-18 16:36:04 +0000

    Write asynchronous function to record score

--- src/apiScore.js
+++ src/apiScore.js
@@ -10,3 +10,22 @@ const allScores = async () => {
       return error.message;
     }
   };
+
+
+const recordScore = async (name, score) => {
+  const params = {};
+  params.user = name;
+  params.score = score;
+  try {
+    const response = await fetch(`${url}/games/${gameID}/scores/`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(params),
+    });
+    return response.json();
+  } catch (error) {
+    return error.message;
+  }
+}
+
+export {allScores, recordScore};
