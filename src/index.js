c5c93785d31379f643333c966c265f9ea8763eed
george-shammar <george.gbenle@outlook.com>
2021-06-18 16:59:18 +0000

    Add leaderboard scene to index

--- src/index.js
+++ src/index.js
@@ -23,6 +23,7 @@ class Game extends Phaser.Game {
     this.scene.add('Credits', CreditsScene);
     this.scene.add('Game', GameScene);
     this.scene.add('BattleScene', BattleScene);
+    this.scene.add('Leaderboard', LeaderboardScene);
     this.scene.add('UIScene', UIScene);
     this.scene.start('Boot');
   }
