65608158a63b993a38c146b76392a848859fd4db
george-shammar <george.gbenle@outlook.com>
2021-06-18 16:41:11 +0000

    Create LeaderboardScene

---
+++ src/Scenes/LeaderboardScene.js
@@ -0,0 +1,13 @@
+import 'phaser';
+
+
+export default class LeaderboardScene extends Phaser.Scene {
+    constructor () {
+      super('Leaderboard');
+    }
+
+
+    create() {
+        
+    }
+}  
