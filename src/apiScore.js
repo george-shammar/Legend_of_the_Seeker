cd2656f82154215849de4398c60aeb27742aadfc
george-shammar <george.gbenle@outlook.com>
2021-06-18 16:04:18 +0000

    Write asynchronous function to get all scores

--- src/apiScore.js
+++ src/apiScore.js
@@ -1,2 +1,12 @@
 const gameID = 'PvttOFDfJUi8zbItrwFO';
-const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api';
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
+
+const allScores = async () => {
+    try {
+      const response = await fetch(`${url}/games/${gameID}/scores`);
+      const rawData = await response.json();
+      return rawData.result;
+    } catch (error) {
+      return error.message;
+    }
+  };
