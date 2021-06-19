fa163ed2dbb140252fd3288e109d1c1e8b6280ff
george-shammar <george.gbenle@outlook.com>
2021-06-19 20:35:59 +0000

    Refactor code

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -144,7 +144,7 @@ export class GameScene extends Phaser.Scene {
     {
       this.physics.pause();
       player.anims.play('turn');
-      // this.scene.switch('BattleScene');
+      this.scene.switch('BattleScene');
     }
 };
 
