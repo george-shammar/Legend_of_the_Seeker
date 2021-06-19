5fa76a61f5a0cfb2068444797bd5cf8d4032317d
george-shammar <george.gbenle@outlook.com>
2021-06-19 11:13:56 +0000

    Create score form

--- src/scoreForm.js
+++ src/scoreForm.js
@@ -5,6 +5,7 @@ const form = () => {
     formLabel.innerHTML = 'Enter Your Username Here To Save Your Score';
     const lineBreak = document.createElement('br');
     formLabel.appendChild(lineBreak);
+
     formHead.appendChild(formLabel);
 
     const scoreInput = document.createElement('input');
@@ -13,7 +14,14 @@ const form = () => {
     scoreInput.setAttribute('name', 'score');
     scoreInput.setRangeText('placeholder', 'Enter your username here');
 
-    
+    formHead.appendChild(scoreInput);
+
+    const submitForm = document.createElement('input');
+    submitForm.setAttribute('type', 'submit');
+    submitForm.setAttribute('value', 'Save Score');
+
+    formHead.appendChild(submitForm);
+
 }
 
 <form>
