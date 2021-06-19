106c91c2074710b44ad02ba6cc9b2eeafd928d31
george-shammar <george.gbenle@outlook.com>
2021-06-19 11:29:49 +0000

    Import allScores and recordScore in gameover scene

--- src/Scenes/GameOverScene.js
+++ src/Scenes/GameOverScene.js
@@ -1,7 +1,12 @@
 import 'phaser';
+import {allScores, recordScore} from '../apiScore';
 
 export class GameScene extends Phaser.Scene {
     constructor () {
       super('GameOver');
+    }
+
+    create() {
+
     }
 }
