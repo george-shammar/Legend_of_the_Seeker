e6a8aa682513eb8e2e90c95bc697099f81ef2b0d
george-shammar <george.gbenle@outlook.com>
2021-06-19 11:10:54 +0000

    Create score form

--- src/scoreForm.js
+++ src/scoreForm.js
@@ -1,3 +1,24 @@
 const form = () => {
+    const formHead = document.createElement('form');
+    const formLabel = document.createElement('label');
+    formLabel.setAttribute('for', 'score');
+    formLabel.innerHTML = 'Enter Your Username Here To Save Your Score';
+    const lineBreak = document.createElement('br');
+    formLabel.appendChild(lineBreak);
+    formHead.appendChild(formLabel);
+
+    const scoreInput = document.createElement('input');
+    scoreInput.setAttribute('type', 'text');
+    scoreInput.setAttribute('id', 'score');
+    scoreInput.setAttribute('name', 'score');
+    scoreInput.setRangeText('placeholder', 'Enter your username here');
+
     
 }
+
+<form>
+    <label for="fname">Enter Your Username Here To Save Your Score</label><br>
+
+    <input type="text" id="fname" name="fname" placeholder="Enter your username">
+    <input type="submit" value="Save Score">
+  </form> 
