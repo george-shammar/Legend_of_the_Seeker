8e77928575deba53de7b52445e374923b9d94272
george-shammar <george.gbenle@outlook.com>
2021-06-21 15:58:16 +0000

    Add control button to instruction scene

--- src/Scenes/InstructionScene.js
+++ src/Scenes/InstructionScene.js
@@ -13,5 +13,13 @@ export default class InstructionScene extends Phaser.Scene {
           align: 'center',
         });
         this.title.setOrigin(0.5);
+
+        this.title = this.add.text(630, 400, 'GAME CONTROLS:', {
+            fontSize: 32,
+            fontStyle: 'bold',
+            color: 'white',
+            align: 'center',
+          });
+        this.title.setOrigin(0.5);
     }
 }
