69697a546ec9f23cf462b10d94d720f854dabe00
george-shammar <george.gbenle@outlook.com>
2021-06-21 03:31:06 +0000

    Add highest scores to Title scene

--- src/Scenes/TitleScene.js
+++ src/Scenes/TitleScene.js
@@ -16,7 +16,10 @@ export default class TitleScene extends Phaser.Scene {
    
     // Credits
     this.creditsButton = new Button(this, config.width/2, config.height/2 + 100, 'blueButton1', 'blueButton2', 'Credits', 'Credits');
-   
+    
+    //leader board
+    this.creditsButton = new Button(this, config.width/2, config.height/2 + 200, 'blueButton1', 'blueButton2', 'Top Scores', 'Leaderboard');
+    
     this.model = this.sys.game.globals.model;
     if (this.model.musicOn === true && this.model.bgMusicPlaying === false) {
       this.bgMusic = this.sound.add('bgMusic', { volume: 0.5, loop: true });
