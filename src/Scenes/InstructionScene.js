0dd0f8bd04d68a12208fd05f025a9a2ab8e5f50d
george-shammar <george.gbenle@outlook.com>
2021-06-21 15:55:29 +0000

    Add instrcution button to title scene

--- src/Scenes/InstructionScene.js
+++ src/Scenes/InstructionScene.js
@@ -4,4 +4,14 @@ export default class InstructionScene extends Phaser.Scene {
     constructor () {
       super('Instruction');
     }
+
+    create() {
+        this.title = this.add.text(630, 30, 'GAME INSTRUCTIONS:', {
+          fontSize: 32,
+          fontStyle: 'bold',
+          color: 'white',
+          align: 'center',
+        });
+        this.title.setOrigin(0.5);
+    }
 }
