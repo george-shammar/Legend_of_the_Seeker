036d06a1d0edcaf98544f82e06a7ecb66454d358
george-shammar <george.gbenle@outlook.com>
2021-06-21 14:03:31 +0000

    Add test for apiScore.js

--- src/apiScore.js
+++ src/apiScore.js
@@ -1,3 +1,4 @@
+import 'regenerator-runtime/runtime';
 const gameID = 'PvttOFDfJUi8zbItrwFO';
 const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
 
--- src/test/apiScore.test.js
+++ src/test/apiScore.test.js
@@ -4,3 +4,20 @@
 
  import fetchMock from 'jest-fetch-mock';
  import {allScores, recordScore} from '../apiScore';
+
+ describe('apiScore', () => {
+    fetchMock.enableMocks();
+    beforeEach(() => {
+      fetch.resetMocks();
+    });
+
+    describe('allScores', () => {
+        it('should be defined', async () => {
+          fetch.mockResponseOnce(JSON.stringify({ result: [{ score: 8000, user: 'Django' }] }));
+          const response = await allScores();
+          expect(response).toBeDefined();
+        });
+    });
+
+
+});
