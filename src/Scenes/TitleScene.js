449199d067b59f32ec597f4d61c912938453e081
george-shammar <george.gbenle@outlook.com>
2021-06-21 03:27:30 +0000

    Change display for form

--- src/Scenes/TitleScene.js
+++ src/Scenes/TitleScene.js
@@ -9,7 +9,7 @@ export default class TitleScene extends Phaser.Scene {
 
   create () {
     // Game
-    this.gameButton = new Button(this, config.width/2, config.height/2 - 100, 'blueButton1', 'blueButton2', 'Play', 'GameOver');
+    this.gameButton = new Button(this, config.width/2, config.height/2 - 100, 'blueButton1', 'blueButton2', 'Play', 'Game');
    
     // Options
     this.optionsButton = new Button(this, config.width/2, config.height/2, 'blueButton1', 'blueButton2', 'Options', 'Options');
