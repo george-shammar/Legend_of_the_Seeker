10428f45f33631a9204b2aa90e0b938d7c70173f
george-shammar <george.gbenle@outlook.com>
2021-06-21 13:38:27 +0000

    test submit button

--- src/test/scoreForm.test.js
+++ src/test/scoreForm.test.js
@@ -28,10 +28,23 @@ describe('form', () => {
     expect(input.innerHTML).toContain("");
   });
 
-  test('Should NOT contain any input before users input from the form function', () => {
+  test('Should Not contain DOM element before users input', () => {
     document.body.innerHTML = '<input type="text" id="score" class="py-2" placeholder="Enter your username here"></input>';
     const input = document.getElementById('score');
     expect(input.innerHTML).not.toContain("username");
   });
+
+  test('Should NOT contain any input before users input from the form function', () => {
+    document.body.innerHTML = '<input type="text" id="score" class="py-2" placeholder="Enter your username here"></input>';
+    const input = document.getElementById('score');
+    expect(input.innerHTML).not.toBeTruthy();
+  });
+
+  test('Should have a submit button for user to confirm input', () => {
+    document.body.innerHTML = '<button class="btn butoon btn-primary mx-2" id="click-submit">Save Score</button>';
+    const submit = document.getElementById('click-submit');
+    expect(input.innerHTML).toContain("");
+  });
+
 
 });
