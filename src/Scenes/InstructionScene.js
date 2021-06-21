66a6307e48081e09e9262fbd3669c14648e57c26
george-shammar <george.gbenle@outlook.com>
2021-06-21 16:08:42 +0000

    Add objevtive to game

--- src/Scenes/InstructionScene.js
+++ src/Scenes/InstructionScene.js
@@ -16,9 +16,9 @@ export default class InstructionScene extends Phaser.Scene {
 
         this.objective1 = this.add.text(200, 60, '1. Scene One: Gather as many fruits (points) as possible without hitting the spike balls', { fontSize: '18px', fill: '#fff' });
         this.objective2 = this.add.text(200, 90, '2. Once you get hit by the spike balls, you get thrown into the Battle Scene(Scene Two) to fight the Dragons', { fontSize: '18px', fill: '#fff' });
-        this.objective3 = this.add.text(200, 90, '2. The difficulty increases with increasing number of fruits gathered. More spike balls are added.', { fontSize: '18px', fill: '#fff' });
-        
-        
+        this.objective3 = this.add.text(200, 120, '3. The difficulty increases with increasing number of fruits gathered. More spike balls are added.', { fontSize: '18px', fill: '#fff' });
+        this.objective4 = this.add.text(200, 150, '4. Defeat the two Dragons in the Battle Scene and you win. Remember to maintain a high score.', { fontSize: '18px', fill: '#fff' });
+        this.objective4 = this.add.text(200, 150, '5. Each time a Dragon attacks, you loose 300 points. You gain 100 points each you attack the Dragons', { fontSize: '18px', fill: '#fff' });
         
         this.title = this.add.text(630, 400, 'GAME CONTROLS', {
             fontSize: 32,
