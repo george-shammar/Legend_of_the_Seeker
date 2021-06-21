13f40e3db31d59db4233ccee7415dc6a3d936060
george-shammar <george.gbenle@outlook.com>
2021-06-21 14:21:31 +0000

    Update test

--- src/test/apiScore.test.js
+++ src/test/apiScore.test.js
@@ -36,6 +36,13 @@
             expect(response[0].user).toEqual('Django');
         });
 
+        it('should NOT return a response with a wrong name/user property', async () => {
+            fetch.mockResponseOnce(JSON.stringify({ result: [{ score: 8000, user: 'Django' }] }));
+            const response = await allScores();
+            expect(response[0].user).not.toEqual('Hommer');
+        });
+
+
         it('should return a response with a name/user property type string', async () => {
             fetch.mockResponseOnce(JSON.stringify({ result: [{ score: 8000, user: 'Django' }] }));
             const response = await allScores();
