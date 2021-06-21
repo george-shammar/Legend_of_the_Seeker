146c4992576469dd34714732d4e3556391189cd5
george-shammar <george.gbenle@outlook.com>
2021-06-21 16:15:54 +0000

    Add control instructions for world scene

--- src/Scenes/InstructionScene.js
+++ src/Scenes/InstructionScene.js
@@ -18,14 +18,28 @@ export default class InstructionScene extends Phaser.Scene {
         this.objective2 = this.add.text(200, 90, '2. Once you get hit by the spike balls, you get thrown into the Battle Scene(Scene Two) to fight the Dragons', { fontSize: '18px', fill: '#fff' });
         this.objective3 = this.add.text(200, 120, '3. The difficulty increases with increasing number of fruits gathered. More spike balls are added.', { fontSize: '18px', fill: '#fff' });
         this.objective4 = this.add.text(200, 150, '4. Defeat the two Dragons in the Battle Scene and you win. Remember to maintain a high score.', { fontSize: '18px', fill: '#fff' });
-        this.objective4 = this.add.text(200, 150, '5. Each time a Dragon attacks, you loose 300 points. You gain 100 points each you attack the Dragons', { fontSize: '18px', fill: '#fff' });
+        this.objective5 = this.add.text(200, 180, '5. Each time a Dragon attacks, you loose 300 points. You gain 100 points each you attack the Dragons', { fontSize: '18px', fill: '#fff' });
+        this.objective6 = this.add.text(200, 210, '6. Register your total score to Leader Baord after the game', { fontSize: '18px', fill: '#fff' });
         
-        this.title = this.add.text(630, 400, 'GAME CONTROLS', {
+        this.title = this.add.text(630, 300, 'GAME CONTROLS', {
             fontSize: 32,
             fontStyle: 'bold',
             color: 'white',
             align: 'center',
           });
         this.title.setOrigin(0.5);
+
+        this.title = this.add.text(630, 330, 'World Scene(First Scene)', {
+            fontSize: 24,
+            fontStyle: 'bold',
+            color: 'white',
+            align: 'center',
+          });
+        this.title.setOrigin(0.5);
+
+
+        this.control1 = this.add.text(200, 360, '1. Move Forward = Right Key (Keyboard)', { fontSize: '18px', fill: '#fff' });
+        this.control2 = this.add.text(200, 390, '2. Move Backward = Left Key (Keyboard)', { fontSize: '18px', fill: '#fff' });
+        this.control3 = this.add.text(200, 420, '3. Jump = Up Key (Keyboard)', { fontSize: '18px', fill: '#fff' });
     }
 }
