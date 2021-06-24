a97adae8426086ee447fee3350262ffa781ac1c5
george-shammar <george.gbenle@outlook.com>
2021-06-24 16:50:36 +0000

    Fix linter error

--- src/Scenes/TitleScene.js
+++ src/Scenes/TitleScene.js
@@ -8,7 +8,6 @@ export default class TitleScene extends Phaser.Scene {
   }
 
   create() {
-
     // Instructions
     this.instr = new Button(this, config.width / 2, config.height / 2 - 200, 'blueButton1', 'blueButton2', 'Instructions', 'Instruction');
 
