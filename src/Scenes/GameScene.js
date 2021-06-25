0c2b9bddb33fec72b0471b7b6e6e07c73d287d21
george-shammar <george.gbenle@outlook.com>
2021-06-25 17:03:56 +0000

    Fix game replay

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -12,9 +12,19 @@ export class GameScene extends Phaser.Scene {
   }
 
   create() {
+    const resfreshSCore = () => {
+      if (window.localStorage) {
+        if (!localStorage.getItem('firstLoad')) {
+          localStorage.firstLoad = true;
+          window.location.reload();
+        } else localStorage.removeItem('firstLoad');
+      }
+    };
+  resfreshSCore();
     /* eslint-disable prefer-const */
     let mud;
     let groundmiddle;
+
 
     this.add.image(650, 300, 'world');
 
